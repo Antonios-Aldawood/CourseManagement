c4b53a8bcfdbef02e04c4ae24d844e8b368b03bd
HP <antonios.dawood00@gmail.com>
2026-01-04 11:12:57 +0300

    Enrollments waiting confirmation added to enrollments aggregate (Wrong, prior design is better, I knew it from the start and already have prepared for it, but have forgotten after a while from touching this project 😅)

---
+++ src/CourseManagement.Domain/Enrollments/EnrollmentWaitingConfirmation.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ErrorOr;
+
+namespace CourseManagement.Domain.Enrollments
+{
+    public class EnrollmentWaitingConfirmation
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int CourseId { get; private set; }
+        public bool IsOptional { get; private set; } = true;
+        public bool IsCompleted { get; private set; } = false;
+        public bool IsConfirmed { get; private set; } = false;
+
+        private EnrollmentWaitingConfirmation(
+            int userId,
+            int courseId)
+        {
+            UserId = userId;
+            CourseId = courseId;
+        }
+
+        public void ConfirmEnrollment()
+        {
+            IsConfirmed = true;
+        }
+
+        private ErrorOr<Success> CheckIfUserCourseEnrollmentWaitingConfirmationIsValid()
+        {
+            if (UserId <= 0 ||
+                CourseId <= 0)
+            {
+                return EnrollmentErrors.IdCannotBeEqualToNorBelowZero;
+            }
+
+            return Result.Success;
+        }
+
+        public static ErrorOr<EnrollmentWaitingConfirmation> CreateEnrollmentWaitingConfirmation(
+            int userId,
+            int courseId)
+        {
+            var enrollmentWaitingConfirmation = new EnrollmentWaitingConfirmation(
+                userId,
+                courseId);
+
+            if (enrollmentWaitingConfirmation.CheckIfUserCourseEnrollmentWaitingConfirmationIsValid() != Result.Success)
+            {
+                return enrollmentWaitingConfirmation.CheckIfUserCourseEnrollmentWaitingConfirmationIsValid().Errors;
+            }
+
+            return enrollmentWaitingConfirmation;
+        }
+    }
+}
---
+++ src/CourseManagement.Domain/Enrollments/EnrollmentWaitingConfirmationErrors.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ErrorOr;
+
+namespace CourseManagement.Domain.Enrollments
+{
+    public class EnrollmentWaitingConfirmationErrors
+    {
+        public static readonly Error IdCannotBeEqualToNorBelowZero = Error.Validation(
+            "EnrollmentWaitingConfirmation.IdCannotBeEqualToNorBelowZero",
+            "Id can not be equal to, nor lesser than, zero.");
+    }
+}
